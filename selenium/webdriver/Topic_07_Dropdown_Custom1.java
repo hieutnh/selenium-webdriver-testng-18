8ae0b877199601f9a8360f9b6953791e4cfb15fc
hieutnh <hieutnh209@gmail.com>
2020-09-11 20:45:08 +0700

    final topic 07 custom

--- selenium/webdriver/Topic_07_Dropdown_Custom1.java
+++ selenium/webdriver/Topic_07_Dropdown_Custom1.java
@@ -32,10 +32,10 @@ public class Topic_07_Dropdown_Custom1 {
 	JavascriptExecutor jsExecutor;
 	WebDriverWait explicitWait;
 
-
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hieut\\git\\selenium-webdriver-testng-18\\Driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\hieut\\git\\selenium-webdriver-testng-18\\Driver\\chromedriver.exe");
 		driver = new ChromeDriver();
 		explicitWait = new WebDriverWait(driver, 30);
 		jsExecutor = (JavascriptExecutor) driver;
@@ -56,44 +56,66 @@ public class Topic_07_Dropdown_Custom1 {
 		selectItemInDropDown("//span[@id='number-button']", "//li[@class='ui-menu-item']/div", "19");
 		Assert.assertTrue(driver.findElement(By.xpath("//span[contains(text(),'19')]")).isDisplayed());
 
-
 	}
-	
+
 	
 	public void TC_02_Angular() {
 		driver.get("https://bom.to/GjUg3AR");
-		selectItemInDropDown("//ejs-dropdownlist[@id='games']//span[contains(@class, 'e-input-group')]", "//ul[@id='games_options']/li", "American Football");
+		selectItemInDropDown("//ejs-dropdownlist[@id='games']//span[contains(@class, 'e-input-group')]",
+				"//ul[@id='games_options']/li", "American Football");
 		Assert.assertEquals(getHidenText("select[id='games_hidden'] option"), "American Football");
-		selectItemInDropDown("//ejs-dropdownlist[@id='games']//span[contains(@class, 'e-input-group')]", "//ul[@id='games_options']/li", "Golf");
+		selectItemInDropDown("//ejs-dropdownlist[@id='games']//span[contains(@class, 'e-input-group')]",
+				"//ul[@id='games_options']/li", "Golf");
 		Assert.assertEquals(getHidenText("select[id='games_hidden'] option"), "Golf");
 	}
 
-	@Test
+	
 	public void TC_03_React() {
 		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
 		selectItemInDropDown("//div[@role='listbox']", "//div[@role='option']", "Jenny Hess");
-		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='divider text' and text()='Jenny Hess']")).isDisplayed());
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//div[@class='divider text' and text()='Jenny Hess']")).isDisplayed());
 		selectItemInDropDown("//div[@role='listbox']", "//div[@role='option']", "Christian");
-		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='divider text' and text()='Christian']")).isDisplayed());
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//div[@class='divider text' and text()='Christian']")).isDisplayed());
 
 	}
+
 	
-	@Test
 	public void TC_04_vueJS() {
 		driver.get("https://mikerodham.github.io/vue-dropdowns/");
 		selectItemInDropDown("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']/li", "First Option");
-		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='divider text' and text()='Jenny Hess']")).isDisplayed());
-		selectItemInDropDown("//div[@role='listbox']", "//div[@role='option']", "Christian");
-		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='divider text' and text()='Christian']")).isDisplayed());
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'First Option')]"))
+						.isDisplayed());
+		selectItemInDropDown("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']/li", "Second Option");
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//li[@class='dropdown-toggle' and contains(text(),'Second Option')]"))
+						.isDisplayed());
 	}
-	@Test
+
+	
 	public void TC_05_editAble() {
 		driver.get("http://indrimuska.github.io/jquery-editable-select/");
-		sendkeydropdown("//div[@id='default-place']//input[@class='form-control es-input']", "Audi");
-	
+		sendkeydropdown("//div[@id='default-place']//input", "Audi");
 		Assert.assertEquals(getHidenText("#default-place li.es-visible"), "Audi");
-		selectItemInDropDown("//div[@role='listbox']", "//div[@role='option']", "Christian");
-		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='divider text' and text()='Christian']")).isDisplayed());
+		sendkeydropdown("//div[@id='default-place']//input", "BMW");
+		Assert.assertEquals(getHidenText("#default-place li.es-visible"), "BMW");
+	}
+
+	@Test
+	public void TC_06_Advance() {
+		driver.get("http://multiple-select.wenzhixin.net.cn/examples#basic.html");
+		driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+		String[] months = {"January", "August", "September"};
+		selectMultiInDropDown("//option/parent::select/following-sibling::div", "//option/parent::select/following-sibling::div//label/span", months);
+		Assert.assertTrue(checkMultiItemSelected(months));
+		
+		driver.navigate().refresh();
+		driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+		String[] months2 = {"March", "April", "September", "December"};
+		selectMultiInDropDown("//option/parent::select/following-sibling::div", "//option/parent::select/following-sibling::div//label/span", months2);
+		Assert.assertTrue(checkMultiItemSelected(months2));
 	}
 
 	@AfterClass
@@ -101,48 +123,106 @@ public class Topic_07_Dropdown_Custom1 {
 		driver.quit();
 	}
 
-	//Hàm cho sendkey
+	// Hàm cho sendkey
 	public void sendkeydropdown(String locator, String value) {
+		driver.findElement(By.xpath(locator)).clear();
+		sleepInSecond(1);
 		driver.findElement(By.xpath(locator)).sendKeys(value);
 		sleepInSecond(1);
-		driver.findElement(By.xpath(locator)).clear();
-		sleepInSecond(1);	
 		driver.findElement(By.xpath(locator)).sendKeys(Keys.TAB);
 		sleepInSecond(1);
+
 	}
-		
-	
-	
+
 	// Hàm được đùng nhiều lần
 	public void selectItemInDropDown(String parentLocator, String ItemLocator, String expectedItem) {
 		// Click vào 1 thẻ bất kì
 		driver.findElement(By.xpath(parentLocator)).click();
-		//Chờ cho tất cả item được xuất hiện
+		// Chờ cho tất cả item được xuất hiện
 		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(ItemLocator)));
-		//Lấy hết tất cả các item đưa vào 1 list
-		List <WebElement> allItems = driver.findElements(By.xpath(ItemLocator));
-		//Tổng số lượng item trong 1 dropdown
+		// Lấy hết tất cả các item đưa vào 1 list
+		List<WebElement> allItems = driver.findElements(By.xpath(ItemLocator));
+		// Tổng số lượng item trong 1 dropdown
 		System.out.println("Item size =" + allItems.size());
-		//Duyệt qua từng item
+		// Duyệt qua từng item
 		for (WebElement item : allItems) {
-		//Verify với text mình cần click
-			
-			if(item.getText().equals(expectedItem)) {
+			// Verify với text mình cần click
+
+			if (item.getText().equals(expectedItem)) {
 				// Scroll truowcs khi click
 				jsExecutor.executeScript("arguments[0].scrollIntoView(true);", item);
 				item.click();
 				sleepInSecond(1);
 				break;
 			}
-		}	
+		}
 	}
-	
-	public String getHidenText(String csslocator) {
-		return (String) jsExecutor.executeScript("return document.querySelector(\"" + csslocator + "\").text");
+	// Dùng có advance Multi Dropdown
+	public void selectMultiInDropDown(String locatorMulti, String itemLocatorMulti, String[] ExpectedMulti) {
+		// Click vào 1 thẻ bất kì
+		driver.findElement(By.xpath(locatorMulti)).click();
+		//Chờ cho tất cả item được xuất hiện
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(itemLocatorMulti)));
+		//Lấy hết tất cả các item đưa vào 1 list
+		List <WebElement> allItems = driver.findElements(By.xpath(itemLocatorMulti));
+		//Tổng số lượng item trong 1 dropdown
+		System.out.println("Item size =" + allItems.size());
+		//Duyệt qua từng item
+		for (WebElement itemMulti : allItems) {
+			//Duyệt qua từng item trong mảng String[]
+			for (String itemExpected : ExpectedMulti ) {
+			//Verify với text mình cần click
+				if(itemMulti.getText().equals(itemExpected)) {
+					// Scroll truowcs khi click
+					jsExecutor.executeScript("arguments[0].scrollIntoView(true);", itemMulti);
+					sleepInSecond(2);
+					//Click vào item cần chọn
+					jsExecutor.executeScript("arguments[0].click();", itemMulti);
+					sleepInSecond(2);
+					List <WebElement> ItemSelected = driver.findElements(By.xpath("//li[@class='selected']//input"));
+					System.out.println("Item Selected =" + ItemSelected.size());
+					if (ExpectedMulti.length == ItemSelected.size()) {
+						break;
+					}
+				}
+			}
+		}
 	}
-	
 
+	//Verify Multi Dropdown
+	public boolean checkMultiItemSelected(String[] ItemMultiSelected) {
+		List <WebElement> ItemSelected = driver.findElements(By.xpath("//li[@class='selected']//input"));
+		//Số item được chọn = với số item
+		int numberItemSelected = ItemSelected.size();
+		
+		//gettext trong dropdown
+		String allItemSelectedText = driver.findElement(By.xpath("//button[@class='ms-choice']/span")).getText();
+		System.out.println("Text đã chọn =" + allItemSelectedText);
+		
+		if (numberItemSelected <= 3 && numberItemSelected > 0) {
+			for (String item : ItemMultiSelected) {
+				if (allItemSelectedText.contains(item)) {
+					break;
+				}
+			}
+				//đúng và true khi nó chứa hết
+				return true;
+			}
+			//Sai nếu >3 text trong dropdown là số
+			else {
+				return driver.findElement(By.xpath("//button[@class='ms-choice']/span[text()='" + numberItemSelected + " of 12 selected']")).isDisplayed();
+			
+			}
+	}
 	
+	
+	// get hiden text
+	public String getHidenText(String csslocator) {
+		return (String) jsExecutor.executeScript("return document.querySelector(\"" + csslocator + "\").textContent");
+	}
+
+	// ("return document.querySelector(\"" + csslocator + "\").textContent");
+
 	public void sleepInSecond(long time) {
 		try {
 			Thread.sleep(time * 1000);
@@ -150,5 +230,4 @@ public class Topic_07_Dropdown_Custom1 {
 			e.printStackTrace();
 		}
 	}
-	
 }
